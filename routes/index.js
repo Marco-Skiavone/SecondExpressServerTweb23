40a1eb933d16a9c98e4ac71edf977ab927ee2305
Ivan <152974976+IvanLusso@users.noreply.github.com>
2024-01-20 15:15:36 +0100

    Minor changes on Index
    Reformatted all names: now are camelCase (exept route names)

--- routes/index.js
+++ routes/index.js
@@ -2,35 +2,35 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
-const dataset_path = './json/';
+const datasetPath = './json/';
 
 let competitionDict = {
     'name'    : 'competition',
     'model'   : require("../models/competition"),
-    'dataset' : JSON.parse(fs.readFileSync(dataset_path + 'cleaned_competitions.json', 'utf-8')),
+    'dataset' : JSON.parse(fs.readFileSync(datasetPath + 'cleaned_competitions.json', 'utf-8')),
     'isEmpty'  : true
 }
 
 let gameLineupsDict = {
     'name'    : 'game lineups',
     'model' : require("../models/game_lineups"),
-    'dataset' : JSON.parse(fs.readFileSync(dataset_path + 'cleaned_game_lineups.json', 'utf-8')),
+    'dataset' : JSON.parse(fs.readFileSync(datasetPath + 'cleaned_game_lineups.json', 'utf-8')),
     'isEmpty': true
 }
 
 let playerValuationModelDict = {
     'name'    : 'player valuation',
     'model' : require("../models/player_valuations"),
-    'dataset' : JSON.parse(fs.readFileSync(dataset_path + 'cleaned_player_valuations.json', 'utf-8')),
+    'dataset' : JSON.parse(fs.readFileSync(datasetPath + 'cleaned_player_valuations.json', 'utf-8')),
     'isEmpty': true
 }
 
 
 router.get('/insert_mongo', (req, res) =>{
     try {
-        const competitionPromise = load_dataset(competitionDict)
-        const gameLineupsPromise = load_dataset(gameLineupsDict);
-        const playerValuationPromise = load_dataset(playerValuationModelDict)
+        const competitionPromise = loadDataset(competitionDict)
+        const gameLineupsPromise = loadDataset(gameLineupsDict);
+        const playerValuationPromise = loadDataset(playerValuationModelDict)
         Promise.all([competitionPromise, gameLineupsPromise,  playerValuationPromise])
             .then(r => {
                 res.status(200).send('Loaded all dataset.')
@@ -41,7 +41,7 @@ router.get('/insert_mongo', (req, res) =>{
 })
 
 router.get('/insert_competitions', (req, res) =>{
-    load_dataset(competitionDict)
+    loadDataset(competitionDict)
         .then(r => {
             res.status(200).send('Loaded dataset.')
         })
@@ -49,7 +49,7 @@ router.get('/insert_competitions', (req, res) =>{
 })
 
 router.get('/insert_game_lineups', (req, res) =>{
-    load_dataset(gameLineupsDict)
+    loadDataset(gameLineupsDict)
         .then(r => {
             res.status(200).send('Loaded dataset.')
         })
@@ -57,7 +57,7 @@ router.get('/insert_game_lineups', (req, res) =>{
 })
 
 router.get('/insert_player_valuations', (req, res) =>{
-    load_dataset(playerValuationModelDict)
+    loadDataset(playerValuationModelDict)
         .then(r => {
             res.status(200).send('Loaded dataset.')
         })
@@ -68,7 +68,7 @@ router.get('/insert_player_valuations', (req, res) =>{
  * @param modelDict a dict containing model name, the scheme model where to insert data, the dataset and if the dataset
  * is empty
  * */
-const load_dataset = async (modelDict) => {
+const loadDataset = async (modelDict) => {
     if (modelDict.isEmpty) {
         await modelDict.model.insertMany(modelDict.dataset)
         modelDict.isEmpty = false;
