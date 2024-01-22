a9f0771f9eae83390f6fb71f1db11f0bce927c56
Ivan <152974976+IvanLusso@users.noreply.github.com>
2024-01-22 10:55:06 +0100

    Update database.js
    removed automated drop of database at server termination

--- databases/database.js
+++ databases/database.js
@@ -13,27 +13,3 @@ connection = mongoose.connect(mongoDB, {
     .catch(err => {
         console.log('connection to mongodb did not work!' + JSON.stringify(err));
     });
-
-/*
-const disconnectAndDropDatabase = async () => {
-    try {
-        // Drop the database
-        await mongoose.connection.db.dropDatabase();
-        console.log('Database dropped successfully');
-    } catch (error) {
-        console.error('Error dropping database:', error);
-        process.exit(1);
-    } finally {
-        // Close the MongoDB connection
-        await mongoose.connection.close();
-        console.log('MongoDB connection closed');
-        process.exit(0);
-    }
-};
-
-// Listen for process termination events
-process.on('exit', disconnectAndDropDatabase);
-process.on('SIGINT', disconnectAndDropDatabase); // Handles Ctrl+C
-process.on('SIGTERM', disconnectAndDropDatabase); // Handles termination signals
-
- */
